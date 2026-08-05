070cac761c9c03e76066c96f9c8fb1c9cc4e4854
Easton Potokar <contagon6@gmail.com>
2026-08-05 17:35:50 -0400

    Bulk of initial work done by llm, seems to be working

---
+++ python/jrl/__init__.py
@@ -0,0 +1,5 @@
+# ruff: noqa: F401, F403
+from jrl import _core as _core
+from jrl._core import *
+
+__version__ = "1.1.1"
--- python/typecasters.h
+++ python/typecasters.h
@@ -1,11 +1,34 @@
+/*
+    This is a slightly modified version of nanobind's optional.h to support
+    boost::optional in addition to std::optional.
+*/
+/*
+    nanobind/stl/optional.h: type caster for std::optional<...>
+
+    Copyright (c) 2022 Yoshiki Matsuda and Wenzel Jakob
+
+    All rights reserved. Use of this source code is governed by a
+    BSD-style license that can be found in the LICENSE file.
+*/
+
 #pragma once
-#include <pybind11/pybind11.h>
+
+#include <nanobind/nanobind.h>
+#include <nanobind/stl/detail/nb_optional.h>
 
 #include <boost/optional.hpp>
 
-namespace PYBIND11_NAMESPACE {
-namespace detail {
+NAMESPACE_BEGIN(NB_NAMESPACE)
+NAMESPACE_BEGIN(detail)
+
+template <typename T>
+struct remove_opt_mono<boost::optional<T>> : remove_opt_mono<T> {};
+
 template <typename T>
 struct type_caster<boost::optional<T>> : optional_caster<boost::optional<T>> {};
-}  // namespace detail
-}  // namespace PYBIND11_NAMESPACE
+
+template <>
+struct type_caster<boost::none_t> : none_caster<boost::none_t> {};
+
+NAMESPACE_END(detail)
+NAMESPACE_END(NB_NAMESPACE)
